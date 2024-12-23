bb964d5215dff3135377e8567c1053627591bc86
Altair <altair200294@gmail.com>
2024-12-23 15:43:02 +0500

    Player branch initial created

---
+++ Soltify/Soltify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soltify
+//
+//  Created by Альтаир Кабдрахманов on 23.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
